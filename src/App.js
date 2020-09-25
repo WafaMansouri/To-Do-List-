24e2980fe5717964077cb76ec37f8bb609c5981a
Wafa <mansouriwafa@yahoo.com>
2020-09-25 16:22:25 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
+import ButtonAdd from './ButtonAdd';
 import './App.css';
 import Task from './Task';
-function App(props) {
-  let [tasks, setTasks]=useState(["My first task", "My second task"]);
-  let [newTask, setNewTask]=useState("");
+function App() {
+  const [tasks, setTasks]=useState([
+   {
+    id: Math.floor(Math.random() * 100000),
+    text: "My first task"
+  },
+  {
+    id: Math.floor(Math.random() * 100000),
+    text: "My Second task"
+  }]);
+  
+  function addFunction(addedTask){
+    setTasks([...tasks, {text: addedTask, id: Math.floor(Math.random() * 100000)}])
+  }
+  function removeFunction(id){
+    setTasks(tasks.filter(el=>el.id!==id))
+  }
+  function editFunction(id,edited){
+    // let arr2=[...tasks];
+    // arr2.splice(tasks.indexOf(nonEdited),1,edited);
+    // setTasks(arr2);
+    setTasks(tasks.map(el => el.id === id ? {id, text: edited} : el))
+  }
   return (
     <div className="App">
-        <h1 style={{color:'#008B8B',marginBottom: '70px'}}>My To-Do List</h1>
-        <form>
-            <input value={newTask} type="text" style={{fontSize:'1em', width:'200px'}} placeholder="Write your new task" onChange={e=>
-            {setNewTask(e.target.value)}}></input>
-            <button onClick={(e)=>{newTask!=="" && setTasks([...tasks, newTask]);
-            e.preventDefault();setNewTask("")}}>Add Task</button>
-        </form>
-            {tasks.map((el,index)=> <Task elem={el} key={index}/>)}
+      <h1 style={{color:'#008B8B',marginBottom: '70px'}}>My To-Do List</h1>
+        <ButtonAdd addFunction={addFunction}/>
+        {tasks.map((el)=> <Task elem={el} removeFunction={removeFunction} editFunction={editFunction} key={el.id}/>)}
     </div>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-function Task(props) {
-    const [remove, setRemove] = useState(false);
+import PropTypes from 'prop-types';
+function Task({elem, removeFunction, editFunction}) {
+    // const [removed, setRemoved] = useState({remove:false, elemRemoved:null});
     const [completed, setCompleted] = useState(false);
     const [textCompleted,setTextCompleted]=useState("Completed");
-    const [editText, setEditText]=useState({edit:false, input:props.elem});
+    const [editText, setEditText]=useState({edit:false, input:elem.text, id:elem.id});
     let styleTask;
     completed ? styleTask = { textDecoration: 'line-through' } : styleTask = {};
     return (
-        !remove &&
-        (<div>
+        <div>
             {!editText.edit? 
             <h2 style={styleTask}>{editText.input}</h2>:
             <div style={{margin:'25px'}}>
@@ -16,11 +16,16 @@ function Task(props) {
                 onChange={e=>setEditText({...editText, input:e.target.value})}></input>
             </div>}
             
-            <button onClick={()=> {setEditText({...editText, edit:!editText.edit})}}>Edit</button>
-            <button onClick={() => setRemove(true)}>Remove</button>
+            <button onClick={()=> {setEditText({...editText, edit:!editText.edit}); editFunction(editText.id,editText.input)}}>Edit</button>
+            <button onClick={() => removeFunction(elem.id)}>Remove</button>
             <button onClick={() => {setCompleted(!completed); completed?
             setTextCompleted("Completed"):setTextCompleted("Uncompleted")}}>{textCompleted}</button>
         </div>)
-    );
+}
+Task.propTypes={elem: PropTypes.exact({
+    id: PropTypes.number,
+    text: PropTypes.string,}),
+    removeFunction:PropTypes.func,
+    editFunction:PropTypes.func,
 }
 export default Task;
